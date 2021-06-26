e54fdc5d58207c48b22047f721187b45cbc37cc9
Yasio <baszczok@yasio.pl>
2021-06-26 22:17:12 +0200

    fix actions

--- src/main.test.ts
+++ src/main.test.ts
@@ -10,17 +10,17 @@ describe('CORE TESTS', () => {
   it('should construct SareClient instance', () => {
     const sc = new SareClient({
       apiKey: '12345678-4321-4321-4321-123456789012',
-      uid: process.env.UID
+      uid: (process.env.UID as string)
     })
     expect(sc.apiKey).toBe('12345678-4321-4321-4321-123456789012')
-    expect(sc.uid).toBe(process.env.UID)
+    expect(sc.uid).toBe((process.env.UID as string))
   });
   
   it('should throw on invalid apikey on SareClient instance construct', () => {
     expect(() => {
       new SareClient({
         apiKey: '12345678-4321-4321-4321-12345678901',
-        uid: process.env.UID
+        uid: (process.env.UID as string)
       })
     }).toThrowError(ERROR_MESSAGES.INAVALID_API_KEY_FORMAT)
   });
@@ -37,7 +37,7 @@ describe('CORE TESTS', () => {
   it('should throw ApiKey Error on set', () => {
     const sc = new SareClient({
       apiKey: '12345678-4321-4321-4321-123456789012',
-      uid: process.env.UID
+      uid: (process.env.UID as string)
     })
     
     expect(() => {sc.apiKey = 'asdfasdf'}).toThrowError(ERROR_MESSAGES.INAVALID_API_KEY_FORMAT)
@@ -46,7 +46,7 @@ describe('CORE TESTS', () => {
   it('should set new apiKey', () => {
     const sc = new SareClient({
       apiKey: '12345678-4321-4321-4321-123456789012',
-      uid: process.env.UID
+      uid: (process.env.UID as string)
     })
     sc.apiKey = '87654321-4321-4321-4321-210987654321'
     expect(sc.apiKey).toBe('87654321-4321-4321-4321-210987654321')
@@ -56,7 +56,7 @@ describe('CORE TESTS', () => {
   it('should throw UID Error on set', () => {
     const sc = new SareClient({
       apiKey: '12345678-4321-4321-4321-123456789012',
-      uid: process.env.UID
+      uid: (process.env.UID as string)
     })
     
     expect(() => {sc.uid = "0"}).toThrowError(ERROR_MESSAGES.INAVALID_UID_FORMAT)
@@ -65,7 +65,7 @@ describe('CORE TESTS', () => {
   it('should set new UID', () => {
     const sc = new SareClient({
       apiKey: '12345678-4321-4321-4321-123456789012',
-      uid: process.env.UID
+      uid: (process.env.UID as string)
     })
     sc.uid = '2001'
     expect(sc.uid).toBe('2001')
@@ -74,7 +74,7 @@ describe('CORE TESTS', () => {
   it('should throw uri Error on set', () => {
     const sc = new SareClient({
       apiKey: '12345678-4321-4321-4321-123456789012',
-      uid: process.env.UID
+      uid: (process.env.UID as string)
     })
     
     expect(() => {sc.uri = "www.invalid.com"}).toThrowError(ERROR_MESSAGES.INAVALID_URI_FORMAT)
@@ -83,7 +83,7 @@ describe('CORE TESTS', () => {
   it('should set new URI without slash', () => {
     const sc = new SareClient({
       apiKey: '12345678-4321-4321-4321-123456789012',
-      uid: process.env.UID
+      uid: (process.env.UID as string)
     })
     sc.uri = 'https://s10.sare.pl/'
     expect(sc.uri).toBe('https://s10.sare.pl')
@@ -92,7 +92,7 @@ describe('CORE TESTS', () => {
   it('should set new URI', () => {
     const sc = new SareClient({
       apiKey: '12345678-4321-4321-4321-123456789012',
-      uid: process.env.UID
+      uid: (process.env.UID as string)
     })
     sc.uri = 'https://s10.sare.pl'
     expect(sc.uri).toBe('https://s10.sare.pl')
@@ -102,8 +102,8 @@ describe('GET EMAIL DATA', () => {
   let sc: SareClient
   beforeEach(() => {
     sc = new SareClient({
-      apiKey: process.env.API_KEY,
-      uid: process.env.UID
+      apiKey: (process.env.API_KEY as string),
+      uid: (process.env.UID as string)
     })
   })
   it('should throw on invalid email', () => {
@@ -123,8 +123,8 @@ describe('ADD/UPDATE EMAIL DATA', () => {
   let sc: SareClient
   beforeEach(() => {
     sc = new SareClient({
-      apiKey: process.env.API_KEY,
-      uid: process.env.UID
+      apiKey: (process.env.API_KEY as string),
+      uid: (process.env.UID as string)
     })
   })
   it('should throw on no emails', () => {
